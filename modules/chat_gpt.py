8bf80e9bd5183d54131b8f0c25e8d7dc097d5abe
dmateusb <dmateusb@unal.edu.co>
2023-07-19 11:02:55 -0500

    refactor, closed: Remove open ai api key

--- modules/chat_gpt.py
+++ modules/chat_gpt.py
@@ -1,12 +1,14 @@
 import openai
 from typing import Text
-
+import os
+from dotenv import load_dotenv
 
 class ChatGPT:
     def __init__(self, model: Text ="gpt-3.5-turbo"):
+        load_dotenv()
         self.__open_ai = openai
         self.__model = model
-        self.__open_ai.api_key = "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"
+        self.__open_ai.api_key = os.getenv("OPEN_AI_API_KEY")
 
     def send_prompt(self, text: Text, callback=None):
         try:
